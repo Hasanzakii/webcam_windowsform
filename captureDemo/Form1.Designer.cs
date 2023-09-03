cd1494123d1629b0070c6480796ca60dfde5d51c
Hassanzakii <hasanzaki2030@gmail.com>
2023-09-03 19:40:49 +0330

    project take photo with system webcam

---
+++ captureDemo/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace captureDemo
+{
+    using System.Drawing.Imaging;
+    using System.Drawing;
+    using System;
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(330, 133);
+            button1.Name = "button1";
+            button1.Size = new Size(134, 41);
+            button1.TabIndex = 0;
+            button1.Text = "start\r\n";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(330, 250);
+            button2.Name = "button2";
+            button2.Size = new Size(134, 39);
+            button2.TabIndex = 1;
+            button2.Text = "snapshot";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(27, 51);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(279, 250);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(489, 51);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(299, 250);
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 20);
+            label1.TabIndex = 4;
+            label1.Text = "webcam";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(489, 28);
+            label2.Name = "label2";
+            label2.Size = new Size(151, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Image Capture Result";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(330, 76);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(134, 28);
+            comboBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox1;
+    }
+}
---
+++ captureDemo/Program.cs
@@ -0,0 +1,21 @@
+namespace captureDemo
+
+{
+    using System.Drawing.Imaging;
+    using System.Drawing;
+    using System;
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
